1b43f30d470654cf1d7b055f14708508027032b5
Pete Lee <pete.a.lee@gmail.com>
2013-03-19 19:01:34 +0000

    allow debug flag/level to be passed in

--- build.sh
+++ build.sh
@@ -4,7 +4,19 @@ TPWD=`pwd`
 
 cd /deepfs/src/flxsmb
 ant clean
-ant -Djavac.debug=on -Djavac.debugLevel=lines,vars,source jar
+if [[ $DEBUG ]]
+then
+    echo "-----------------------------"
+    echo "@@@ BUILDING FLXSMB DEBUG @@@"
+    echo "-----------------------------"
+    ant -Djavac.debug=on -Djavac.debugLevel=lines,vars,source jar
+else
+    echo "-------------------------------"
+    echo "@@@ BUILDING FLXSMB RELEASE @@@"
+    echo "-------------------------------"
+    ant jar
+fi
+
 cp -f /deepfs/src/flxsmb/dist/jcifs-1.3.17-flx.1.jar /deepfs/src/siqsmb/lib/jcifs-1.3.17-flx.1.jar
 
 cd $TPWD
